cefcf5d9b02d6f1e087a77868d7206ec4c47f5e1
dimkaprodimka <dimkaprodimka@aol.com>
2022-09-08 15:24:53 +0200

    add enter user acestream server ip for make playlist for vlc

--- tvfinder.py
+++ tvfinder.py
@@ -3,7 +3,7 @@ import requests
 from sys import argv
 
 channel = argv[1]
-
+user_ip = input('input ip acestream server for playlist 127.0.0.1 or remoteIP: ')
 def get_tv_link(channel):
     url = f'https://acestreamsearch.net/?q={channel}'
     content = requests.get(url).text
@@ -27,10 +27,12 @@ ln, lh = get_tv_link(channel)
 #change http://38.242.159.189:6878 to you ip blaiseio/acelink server or docker
 
 pl = '''#EXTINF:-1 group-title="TV",{}
-http://38.242.159.189:6878/ace/getstream?id={}    
+http://{}:6878/ace/getstream?id={}
 '''
 
 with open(channel+'.m3u', 'w') as f:
     f.write('#EXTM3U\n')
     for a in range(len(ln)):
-        f.write(pl.format(ln[a], lh[a][12:]))
+        f.write(pl.format(ln[a], user_ip, lh[a][12:]))
+
+print(user_ip)
