21597ee21e92bbe551f0a58c8dc74a7d886a9fcc
dimkaprodimka <dimkaprodimka@aol.com>
2023-03-02 23:09:56 +0100

    add Dockerfile and docker auto version with args param, edit README

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
 # acestream playlist creator
+Use:
+tvfinder-docker.py <channel or preffix like [pl]> <format playlist: ace,vlc> <ip acestream-server, or none>
+example:
+python3 tvfinder-docker.py hbo ace n
+python3 tvfinder-docker.py hbo vlc 127.0.0.1
 
-python3 tvfinder.py "prefix-3word"
+Docker:
+docker build -t tvfinder . 
+docker run --rm -a stdout --name tv tvfinder:latest hbo vlc 127.0.0.1    "or add"  > channel.m3u
+
+
+python3 tvfinder.py <channel or prefix>
+example: 
+python3 tvfinder.py sport
+
 select format playlist acestream 
 or m3u for vlc and iptv, next write ip addr acestream server (default localhost: 127.0.0.1)
 
---
+++ tvfinder-docker.py
@@ -0,0 +1,53 @@
+from bs4 import BeautifulSoup
+import requests
+from sys import argv
+
+channel = argv[1]
+choys = argv[2]
+ip = argv[3]
+
+def get_tv_link(channel):
+    url = f'https://acestreamsearch.net/?q={channel}'
+    content = requests.get(url).text
+    soup = BeautifulSoup(content, 'html.parser')
+    res = soup.find("ul", class_="list-group")
+    a_tags = res.findAll('a')
+
+    list_name = []
+    list_href = []
+
+    for a in a_tags:
+        name = a.text
+        href = a['href']
+#        print('{}\t{}'.format(name, href))
+        list_name.append(name)
+        list_href.append(href)
+    return list_name, list_href
+
+ln, lh = get_tv_link(channel)
+
+if choys == 'ace':
+    pl = '''
+#EXTINF:-1 group-title="{}",{}
+{}
+    '''
+    print('#EXTM3U\n')
+    for a in range(len(ln)):
+        print(pl.format(channel, ln[a],  lh[a]))
+            
+elif choys == 'vlc':
+    user_ip = ip
+
+    pl = '''
+#EXTINF:-1 group-title="{}",{}
+http://{}:6878/ace/getstream?id={}
+    '''
+
+    print('#EXTM3U\n')
+    for a in range(len(ln)):
+        print(pl.format(channel, ln[a], user_ip, lh[a][12:]))
+
+else:
+    print('bad choys bye bye')
+
+
